0da27285e89d94e2f6a275c20469b2b7ad1644ff
SyberLeCyber <decultot.louis@gmail.com>
2018-11-27 17:00:38 +0100

    ex 3

--- exercice-3/prototype.js
+++ exercice-3/prototype.js
@@ -2,4 +2,44 @@ String.prototype.ucfirst = function ucfirst() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
-// console.log("ma chaine".ucfirst());
+String.prototype.vig = function vig(key) {
+    if (typeof key !== "string")
+        return this;
+
+    let cryptedString = "";
+    let space = 0;
+    for (let i = 1; i <= this.length; i++) {
+        if (this[i - 1] === " ") {
+            cryptedString += " ";
+            space++;
+            continue;
+        }
+        const alphabetPositionString = this.toUpperCase().charCodeAt(i - 1) - 65;
+        const alphabetPositionKey = key.toUpperCase().charCodeAt((i - 1 - space) % key.length) - 65;
+        cryptedString += String.fromCharCode(65 + (alphabetPositionString + alphabetPositionKey) % 26);
+    }
+    return cryptedString.toLowerCase();
+};
+//
+// Object.prototype.prop_acces =  function prop_access(path) {
+//     if (path == null || path === '')
+//         return this;
+//     const props = path.split('.');
+//     let property = this;
+//     props.forEach(function (prop) {
+//         if (!property.hasOwnProperty(prop)) {
+//             console.log(path + ' not exist');
+//             return;
+//         }
+//         property = property[prop];
+//     });
+//     return property;
+// };
+//
+// // console.log("ma chaine".ucfirst());
+// // console.log("wikipedia".vig("crypto"));
+//
+// const object = {};
+// object.type = {};
+// object.type.name = "Louis";
+// console.log(object.prop_access("type.name"));
